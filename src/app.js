4efdfd6382ae281f0201e5831f343aec5343902a
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-02-16 20:49:10 +0000

    fix/finalizadas las correcciones

--- src/app.js
+++ src/app.js
@@ -7,4 +7,60 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  /*  //Variables de quien, actión, qué, y cuándo.
+  let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+  let action = ["comió", "orinó", "aplastó", "rompió"];
+  let what = ["mi tarea", "las llaves", "el auto"];
+  let when = [
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando terminé",
+    "durante mi almuerzo",
+    "mientras estaba orando"
+  ];
+
+  //FUNCIONES QUE GENERAN NÚMEROS DE POSITION DE LOS ARRAYS
+  const generadorwho = () => {
+    let resultWHO = Math.floor(Math.random() * who.length);
+    return resultWHO;
+  };
+
+  const generadoraction = () => {
+    let resultACTION = Math.floor(Math.random() * action.length);
+    return resultACTION;
+  };
+
+  const generadorwhat = () => {
+    let resultWHAT = Math.floor(Math.random() * what.length);
+    return resultWHAT;
+  };
+
+  const generadorwhen = () => {
+    let resultWHEN = Math.floor(Math.random() * when.length);
+    return resultWHEN;
+  };
+
+  //FUNCION QUE ACUMULAR Y DAR COMO RESULTADO LA PALABRA DE LOS DISTINTOS ARRAYS
+  function acumuladorDeGeneradores(n1, n2, n3, n4) {
+    let resutALL = [who[n1], action[n2], what[n3], when[n4]];
+    console.log(resutALL.join(" "));
+
+    document.getElementById("excuse").innerHTML =
+      "<h2>La excusa es:</h2><div class='alert alert-success'><h4>" +
+      who[n1] +
+      " " +
+      action[n2] +
+      " " +
+      what[n3] +
+      " " +
+      when[n4] +
+      "</h4></div>";
+  }
+
+  acumuladorDeGeneradores(
+    generadorwho(),
+    generadoraction(),
+    generadorwhat(),
+    generadorwhen()
+  ); */
 };
